4a9bf9371e5c6e1a8fbd958eb89c1a4cf9c44181
safakselimsofioglu <safak.sofioglu@icloud.com>
2024-11-29 21:09:24 +0300

    Initial Commit

---
+++ Greetings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Greetings
+//
+//  Created by Şafak Selim Sofioğlu on 29.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Şafak Selim Sofioğlu on 29.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
